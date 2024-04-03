026750159424e0b6ca43fb934554c7bdd32e5f6c
ginohmk <michotall95@gmail.com>
2024-04-03 10:40:41 +0100

    feat: add general route logic

---
+++ router/booksdb.js
@@ -0,0 +1,54 @@
+let books = {
+  1: {
+    isbn: '11',
+    author: 'Chinua Achebe',
+    title: 'Things Fall Apart',
+    reviews: {},
+  },
+  2: {
+    isbn: '12',
+    author: 'Hans Christian Andersen',
+    title: 'Fairy tales',
+    reviews: {},
+  },
+  3: {
+    isbn: '13',
+    author: 'Dante Alighieri',
+    title: 'The Divine Comedy',
+    reviews: {},
+  },
+  4: {
+    isbn: '14',
+    author: 'Unknown',
+    title: 'The Epic Of Gilgamesh',
+    reviews: {},
+  },
+  5: { isbn: '15', author: 'Unknown', title: 'The Book Of Job', reviews: {} },
+  6: {
+    isbn: '16',
+    author: 'Unknown',
+    title: 'One Thousand and One Nights',
+    reviews: {},
+  },
+  7: { isbn: '17', author: 'Unknown', title: "Nj\u00e1l's Saga", reviews: {} },
+  8: {
+    isbn: '18',
+    author: 'Jane Austen',
+    title: 'Pride and Prejudice',
+    reviews: {},
+  },
+  9: {
+    isbn: '19',
+    author: 'Honor\u00e9 de Balzac',
+    title: 'Le P\u00e8re Goriot',
+    reviews: {},
+  },
+  10: {
+    isbn: '110',
+    author: 'Samuel Beckett',
+    title: 'Molloy, Malone Dies, The Unnamable, the trilogy',
+    reviews: {},
+  },
+};
+
+module.exports = books;
---
+++ router/general.js
@@ -0,0 +1,70 @@
+const express = require('express');
+let books = require('./booksdb.js');
+let isValid = require('./auth_users.js').isValid;
+let users = require('./auth_users.js').users;
+const public_users = express.Router();
+
+public_users.post('/register', (req, res) => {
+  //Write your code here
+  const { username, password } = req.body;
+
+  if (username && password) {
+    if (!isValid) {
+      users.push({ username, password });
+      return res
+        .status(200)
+        .json({ message: 'User successffuly registered!!' });
+    } else {
+      return res.status(404).json({ messsage: 'User already exist' });
+    }
+  }
+
+  return res.status(404).json({ message: 'Unable to register ' });
+});
+
+// Get the book list available in the shop
+public_users.get('/', function (req, res) {
+  //Write your code here
+
+  if (users.length === 0) {
+    res.status(201).json({ message: 'No user avialable' });
+  } else {
+    return res.status(200).json({ data: users });
+  }
+});
+
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn', function (req, res) {
+  //Write your code here
+  const { isbn } = req.params;
+
+  const filteredBooks = books.filter((book) => book.isbn === isbn);
+  return res.status(300).json({ data: filteredBooks });
+});
+
+// Get book details based on author
+public_users.get('/author/:author', function (req, res) {
+  //Write your code here
+  const { author } = req.params;
+  const filteredAuthor = books.filter((book) => book.author === author);
+  return res.status(300).json({ data: filteredAuthor });
+});
+
+// Get all books based on title
+public_users.get('/title/:title', function (req, res) {
+  //Write your code here
+  const { title } = req.params;
+  const filteredTitle = books.filter((book) => book.title === title);
+  return res.status(300).json({ data: filteredTitle });
+});
+
+//  Get book review
+public_users.get('/review/:isbn', function (req, res) {
+  //Write your code here
+  const { isbn } = req.params;
+  const filteredTitle = books.filter((book) => book.isbn === isbn);
+
+  return res.status(300).json({ data: filteredTitle.reviews });
+});
+
+module.exports.general = public_users;
